3f207d7975a8026c3b43c7518721525dd3dbfc06
maghiarul <panaite.cristian99@gmail.com>
2022-10-29 17:39:30 +0300

    bruv

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import About from "../public/about.png";
+import Image from "next/image";
 
 function about() {
   return (
-    <div className='page'>
-        <Navbar />
-        <h1>something here</h1>
+    <div className="page">
+      <Navbar />
+      <div className="about-content">
+        <div className="about-heading">
+          <h1>From shepherd to 6-figure UX solo designer</h1>
+        </div>
+        <div className="about-image">
+          <Image src={About} alt="about photo" width={650} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default about
+export default about;
